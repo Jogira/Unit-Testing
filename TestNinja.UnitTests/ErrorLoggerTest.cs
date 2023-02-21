a038a57ba7196535f0b3c43fbaabb2fc652696d0
Jonathan <Zweitonegoismus@gmail.com>
2023-02-21 15:01:59 -0500

    Add tests for raised events.

--- TestNinja.UnitTests/ErrorLoggerTest.cs
+++ TestNinja.UnitTests/ErrorLoggerTest.cs
@@ -23,12 +23,22 @@ namespace TestNinja.UnitTests
         [TestCase(null)]
         [TestCase("")]
         [TestCase(" ")]
-       public void Log_InvalidError_ThrowArgumentNullException(string error)
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
         {
             var logger = new ErrorLogger();
             //logger.Log(error);
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
             //Assert.That(() => logger.Log(error), Throws.Exception.TypeOf<DivideByZeroException>);
+        }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args;  };
+            logger.Log("a");
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
         }
     }
 }
