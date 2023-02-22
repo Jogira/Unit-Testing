4bc88cf898a6cb01ab7e5ec483a6d6e9ef474a8f
Jonathan <Zweitonegoismus@gmail.com>
2023-02-21 19:02:10 -0500

    Moq test case.

--- TestNinja.UnitTests/FakeFileReader.cs
+++ TestNinja.UnitTests/FakeFileReader.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TestNinja.Mocking;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using TestNinja.Mocking;
 
-namespace TestNinja.UnitTests
-{
-    internal class FakeFileReader : IFileReader
-    {
-        public string Read(string path)
-        {
-            return "";
-        }
-    }
-}
+//namespace TestNinja.UnitTests
+//{
+//    internal class FakeFileReader : IFileReader
+//    {
+//        public string Read(string path)
+//        {
+//            return "";
+//        }
+//    }
+//}
--- TestNinja.UnitTests/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,16 +9,31 @@ using TestNinja.Mocking;
 
 namespace TestNinja.UnitTests.Mocking
 {
+
     [TestFixture]
     internal class VideoServiceTests
     {
+        private Mock<IFileReader> _fileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new FakeFileReader());
+
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            //var service = new VideoService(new FakeFileReader());
             //Replace the REAL file reader with a FAKE one.
             //service.FileReader = new FakeFileReader();
-            var result = service.ReadVideoTitle();
+            var result = _videoService.ReadVideoTitle();
+
             Assert.That(result, Does.Contain("Error").IgnoreCase);
         }
     }
